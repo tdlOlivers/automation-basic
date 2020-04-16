be379d9e0b69f3fddcf743d4dc6acda490a11648
Olivers Oskars Sīpols <olivers.sipols@testdevlab.com>
2020-04-16 22:44:05 +0300

    update

--- setupScript4Linux.sh
+++ setupScript4Linux.sh
@@ -20,6 +20,8 @@ echo 'Unzipping juice-shop'
 tar zxvf juice-shop-10.1.0_node12_linux_x64.tgz
 echo 'Renaming juice-shop'
 mv juice-shop_10.1.0/ juice-shop
+echo 'Removing juice-shop zip'
+rm juice-shop-10.1.0_node12_linux_x64.tgz
 echo 'Cloning automation projects'
 git clone https://github.com/tdlOlivers/web-automation-basic.git
 git clone https://github.com/tdlOlivers/api-automation-basic.git
