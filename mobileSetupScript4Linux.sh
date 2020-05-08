79860fa7cb1601df90243fa771965fee78e234b2
tdlOlivers <olivers.sipols@testdevlab.com>
2020-05-08 08:33:10 +0300

    Update mobileSetupScript4Linux.sh

--- mobileSetupScript4Linux.sh
+++ mobileSetupScript4Linux.sh
@@ -4,23 +4,23 @@ sudo apt update
 sudo apt install snapd
 sudo snap install --classic code # or code-insiders
 sudo snap install android-studio --classic
-sudo npm install -g appium
 sudo apt install git
 sudo apt install npm
 sudo apt install maven
 sudo apt install default-jdk
-echo 'export JAVA_HOME=/usr/lib/jvm/java-1.11.0-openjdk-amd64' >> .bashrc
-echo 'export PATH=$JAVA_HOME/bin:$PATH' >> .bashrc
-echo 'export ANDROID_HOME=$HOME/Android/Sdk' >> .bashrc
-echo 'export PATH=$PATH:$ANDROID_HOME/tools' >> .bashrc
-echo 'export PATH=$PATH:$ANDROID_HOME/platform-tools' >> .bashrc
-source .bashrc
-android-studio
-
+sudo npm install -g appium
+echo 'export JAVA_HOME=/usr/lib/jvm/java-1.11.0-openjdk-amd64' >> ~/.bashrc
+echo 'export PATH=$JAVA_HOME/bin:$PATH' >> ~/.bashrc
+echo 'export ANDROID_HOME=$HOME/Android/Sdk' >> ~/.bashrc
+echo 'export PATH=$PATH:$ANDROID_HOME/tools' >> ~/.bashrc
+echo 'export PATH=$PATH:$ANDROID_HOME/platform-tools' >> ~/.bashrc
+source ~/.bashrc
 wget https://github.com/appium/appium-desktop/releases/download/v1.15.1/Appium-linux-1.15.1.AppImage
 chmod +x Appium-linux-1.15.1.AppImage
 mv Appium-linux-1.15.1.AppImage Desktop/appium-desktop.AppImage
-
 cd Desktop/
 git clone https://github.com/tdlOlivers/mobile-automation-basic.git
 cd mobile-automation-basic/
+mvn compile
+cd
+android-studio
